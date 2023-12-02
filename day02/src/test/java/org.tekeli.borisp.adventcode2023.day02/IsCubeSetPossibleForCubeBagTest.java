3772ca64e25f0db335e27a93b862cb1ffa8e9dcd
borisp <boris.pigin@gmail.com>
2023-12-02 11:12:41 +0100

    day02 - no need to be public

--- day02/src/test/java/org.tekeli.borisp.adventcode2023.day02/IsCubeSetPossibleForCubeBagTest.java
+++ day02/src/test/java/org.tekeli.borisp.adventcode2023.day02/IsCubeSetPossibleForCubeBagTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
-public class IsCubeSetPossibleForCubeBagTest {
+class IsCubeSetPossibleForCubeBagTest {
 
     private IsCubeSetPossibleForCubeBag sus;
 
