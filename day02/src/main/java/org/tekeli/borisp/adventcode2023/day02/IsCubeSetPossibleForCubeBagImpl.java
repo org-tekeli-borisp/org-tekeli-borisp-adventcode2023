4edc235868f19c03c3103ebef16c75c9023efbd9
borisp <boris.pigin@gmail.com>
2023-12-02 11:12:07 +0100

    day02 - protected implementation

--- day02/src/main/java/org/tekeli/borisp/adventcode2023/day02/IsCubeSetPossibleForCubeBagImpl.java
+++ day02/src/main/java/org/tekeli/borisp/adventcode2023/day02/IsCubeSetPossibleForCubeBagImpl.java
@@ -1,6 +1,6 @@
 package org.tekeli.borisp.adventcode2023.day02;
 
-public class IsCubeSetPossibleForCubeBagImpl implements IsCubeSetPossibleForCubeBag {
+class IsCubeSetPossibleForCubeBagImpl implements IsCubeSetPossibleForCubeBag {
     @Override
     public boolean test(final CubeSubset cubeSet, final CubeBag cubeBag) {
         return areRedsPossible(cubeSet, cubeBag) &&
