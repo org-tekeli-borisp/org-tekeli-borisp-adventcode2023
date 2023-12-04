9a4eeda80d28acf01c7123ceef14c99edf583a7b
borisp <boris.pigin@gmail.com>
2023-12-04 14:10:42 +0100

    day02 - Dit and Dat

--- day02/src/main/java/org/tekeli/borisp/adventcode2023/day02/LineToCubeSetsImpl.java
+++ day02/src/main/java/org/tekeli/borisp/adventcode2023/day02/LineToCubeSetsImpl.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class LineToCubeSetsImpl implements LineToCubeSets {
+class LineToCubeSetsImpl implements LineToCubeSets {
 
     private final LineToCubeSet lineToCubeSet;
 
